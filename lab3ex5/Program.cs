d1afa540a5477185f076d086ab73d9f6fc12678d
illei <39960324+illei@users.noreply.github.com>
2023-09-08 09:31:27 +0200

    Update Program.cs

--- lab3ex5/Program.cs
+++ lab3ex5/Program.cs
@@ -6,6 +6,19 @@ namespace lab3ex5
     {
         static void Main(string[] args)
         {
+            /*Arpsod adoră două lucruri: matematica și clătitele bunicii sale. Într-o zi, aceasta s-a apucat să
+prepare clătite. Arpsod mănâncă toate clătitele începând de la a N-a clătită preparată, până
+la a M-a clătită preparată (inclusiv N și M). Pentru că el vrea să mănânce clătite cu diferite
+umpluturi și-a făcut următoarea regulă:
+ Dacă numărul de ordine al clătitei este prim atunci aceasta va fi cu ciocolată.
+ Dacă numărul de ordine este pătrat perfect sau cub perfect aceasta va fi cu gem.
+ Dacă suma divizorilor numărului este egală cu însuși numărul de ordine atunci aceasta va fi cu
+înghețată. (se iau în considerare toți divizorii în afară de numărul în sine, inclusiv 1).
+ Dacă niciuna dintre condițiile de mai sus nu este îndeplinită, pentru cele cu numărul de ordine
+par, clătita va fi cu zahar, iar pentru numărul de ordine impar, clătita va fi simplă.”
+• Cerința
+ Cunoscându-se N și M, numere naturale, să se determine câte clătite a mâncat Arpsod în total și
+numărul de clătite din fiecare tip. A*/
             Console.WriteLine("Cele doua  punce :");
             int n = int.Parse(Console.ReadLine());
             int m = int.Parse(Console.ReadLine());
